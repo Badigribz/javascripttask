6d99067c6cda4db738631293b4992299c3be3a1d
Gribz <gribzbadi@gmail.com>
2024-01-05 18:32:46 +0300

    javascripttask

---
+++ asgn.js
@@ -0,0 +1,198 @@
+x = 5;
+y = 6;
+z = x + y;
+console.log(z)
+
+
+
+var a = 10;
+var b = 11;
+var c = a + b;
+console.log(c)
+
+
+
+
+let d = c + z;
+console.log(d)
+
+
+
+const e = d - 12;
+console.log(e) 
+
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function hello() 
+{
+    console.log ('hello world');
+}
+hello();
+
+
+
+
+
+arrow1 = () => 
+{
+  console.log("Hello World1!");
+}
+arrow1();
+
+
+
+
+
+arrow2 = () => console.log("Hello World2!");
+arrow2();
+
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Templiteral() 
+{   
+    console.log ('Template literals');
+    console.log (`Template literals different exhibit`);
+    console.log (`He's often called "Johnny"`);
+
+}
+Templiteral();
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+
+
+
+
+
+
+//this is a comment
+
+
+/*when you Want 
+to write many lines of comment 
+this is how you do it... */
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+
+
+
+
+
+concatenate = () => {
+    var a = 20;
+    var b = 30;
+    c = a + b;
+    console. log("The answer is:"+c);
+}
+concatenate();
+
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+interpolate = () => {
+    let name = "Badi"; 
+    let string = `My name is ${name}.`; 
+    console.log(string); 
+}
+interpolate();
+
+console.log("\n"); 
+console.log("\n"); 
+
+
+
+
+
+
+
+hoisting = () => {
+    var a;
+    var b;
+    a = 40;
+    b = a + 10;
+
+    c = a + b;
+
+    console.log(c);
+
+}
+hoisting();
+
+
+
+
+
+
+
+
+
+
+
+
+// let text = "This is an example of a string to store or manipulate text";
+// console.log(text)
+
+
+
+
+// let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYASDFG";
+// let length = text1.length;
+// console.log(text1.length)
+
+
+
+
+// let part = text.slice(7, 13); 
+// console.log(part) 
+
+
+
+// let part2 = text. substring(1, 57); 
+// console.log(part2) 
+
+
+
